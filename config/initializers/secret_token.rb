8837ef1490d8bb626b4586607a52576d23f69401
Billy Shih <me@billyshih.com>
2013-05-24 15:43:45 -0700

    Init app and scaffold lists and tasks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowTo::Application.config.secret_token = 'c9110d04cdb4a08ae64a5eed4fcf265d5e4cfd4421db23a58ada1fa1f08c679e17fb9344fda0cdeabcf3b2e17eebdce7847a7c881cc41d3a1c86b638390cc0f2'
